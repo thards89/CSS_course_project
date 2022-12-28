4965a40d505c750861800bb331b669af02804859
thards89 <thards89@gmail.com>
2022-12-28 11:27:08 +0100

    first commit

---
+++ shared.js
@@ -0,0 +1,35 @@
+const backdrop = document.querySelector('.backdrop');
+const modal = document.querySelector('.modal')
+const modalNoButton = document.querySelector('.modal__action--negative')
+const selectPlanButtons = document.querySelectorAll('.plan button');
+const toggleButton = document.querySelector('.toggle-button')
+const mobileNav = document.querySelector('.mobile-nav')
+
+for (let i = 0; i < selectPlanButtons.length; i++){
+    selectPlanButtons[i].addEventListener('click', function(){
+        modal.style.display = 'block';
+        backdrop.style.display = 'block'
+    })
+}
+
+backdrop.addEventListener('click', function() {
+    mobileNav.style.display = 'none';
+    closeModal();
+});
+
+if (modalNoButton) {
+    modalNoButton.addEventListener('click', closeModal);
+}
+
+
+function closeModal() {
+    if (modal) {    
+    modal.style.display = "none";
+}
+    backdrop.style.display = "none";
+}
+
+toggleButton.addEventListener('click', function() {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+})
